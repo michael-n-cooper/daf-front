6c4594c17438f573b203990037f1ed3e421ad22b
Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
2024-06-10 08:54:17 -0400

    Squashed commit of the following:
    commit d25fbeceb7d3a66922d4dfd7941414091d344110
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Sun Jun 2 11:00:49 2024 -0400
    
        counts working
    
    commit 3561baaaa835d81493896763bdb68cb144be5ba6
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Sun Jun 2 10:27:28 2024 -0400
    
        most features re-added, still counts and intersections
    
    commit 1dcad0ee69628350fac0e10a828587b1583253b0
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Sat Jun 1 13:50:14 2024 -0400
    
        Update matrix.js
    
    commit e2aa6352506bf92a5486fb16a5dd436109cfd95f
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Sat Jun 1 13:35:39 2024 -0400
    
        generating dom in separate script
    
    commit 154f7b4c912b9d2a406134bbb4f920995c4e21d1
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Sat Jun 1 07:32:26 2024 -0400
    
        install file-url
    
    commit 15fbfb087da4232f3144cfb1e3c5a012234d5949
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Fri May 31 14:44:48 2024 -0400
    
        basic jsdom working
    
    commit a0bbf53a69879300f614a5ad9bd1c896ecd8a211
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Fri May 31 14:26:08 2024 -0400
    
        install jsdom
    
    commit 69347d10d87bf04e12e84beda8f62491fd95a73c
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Fri May 31 14:25:34 2024 -0400
    
        fetching
    
    commit c5df679b7d989fa4a6a97a6c793aabe8f45ee541
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Thu May 30 08:57:42 2024 -0400
    
        mostly using the api
    
    commit 17fcd899cdb6c68f1b48f7b1b99aea56c3fe77de
    Author: Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
    Date:   Wed May 29 16:50:24 2024 -0400
    
        starting the refactor, mostly index pages, still to do intersection needs

--- src/script/util.js
+++ src/script/util.js
@@ -1,3 +1,3 @@
-export { findObjectByProperties, filterObjectByProperties, idFrag, compareStr, mdToHtml, isValidUrl, getOneProp, getFileData, escSparql } from './util-base.js';
+export { findObjectByProperties, filterObjectByProperties, idFrag, compareStr, mdToHtml, isValidUrl, getOneProp, getFileData, escSparql, apiGet } from './util-base.js';
 
 export const baseUri = import.meta.env.BASE_URL;
