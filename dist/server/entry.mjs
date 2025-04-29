6bd773e6901697893354b93eaf496488bce11cc4
Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
2025-04-29 06:35:41 -0400

    build

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CsuUM6YZ.mjs';
-import { manifest } from './manifest_P6gx2gEK.mjs';
+import { manifest } from './manifest_Hubt0gXW.mjs';
 
 const serverIslandMap = new Map();;
 
